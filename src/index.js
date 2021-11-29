b5f00d10e8d141c928c7a20e4493d64c2292120e
Maxim Salnikov <salnikov@gmail.com>
2021-11-29 23:43:51 +0100

    ga

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import ReactGA from 'react-ga4';
 
-ReactGA.initialize('G-PKNGLJ5FKX');
+ReactGA.initialize('G-0WZN3GNEX8');
 ReactGA.send('pageview');
 
 ReactDOM.render(
